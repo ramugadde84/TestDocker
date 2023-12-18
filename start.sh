5c07be25c648b71a7c59c87fa817f6813b18e0ae
Ramu Gadde <ramugadde84@gmail.com>
2023-12-18 13:35:40 -0600

    updated jenkins file

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+<<<<<<< HEAD
 
 #!/bin/sh
+=======
+>>>>>>> parent of 3f1ff5d (removed start up script)
 java -XX:HeapDumpPath=/heapdumps/custom_heap_dump.hprof -XX:+HeapDumpOnOutOfMemoryError -jar /test-docker-spring-boot.jar
